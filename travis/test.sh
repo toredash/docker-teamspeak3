2fa80bf25e08f997c14e5fade93592501fa93f18
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-02 14:26:47 +0200

    Altmost weekend\!

--- travis/test.sh
+++ travis/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 DOCKER_ID=`docker ps | tail -n +2 | awk '{print $1}'`
 PASSWORD=`docker logs --tail=all $DOCKER_ID 2>&1 | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
 echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
