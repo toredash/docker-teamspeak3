d38885bc40db0a0950d286a5fd4f7040af0c86e6
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-02 14:12:27 +0200

    Wrong IP used

--- travis/test.sh
+++ travis/test.sh
@@ -2,5 +2,5 @@
 
 DOCKER_ID=`docker ps | tail -n +2 | 'awk {print $1}'`
 PASSWORD=`docker logs --tail=all $DOCKER_ID 2>&1 | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
-echo "login serveradmin $PASSWORD" | nc 146.185.152.134 10011  | grep "error id=0 msg=ok"
+echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
 
