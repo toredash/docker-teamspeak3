44d70cb8cb3003e39ab728c1b72dbc9e921cd828
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-02 14:23:01 +0200

    Another stupid typo

--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKER_ID=`docker ps | tail -n +2 | 'awk {print $1}'`
+DOCKER_ID=`docker ps | tail -n +2 | awk '{print $1}'`
 PASSWORD=`docker logs --tail=all $DOCKER_ID 2>&1 | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
 echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
 
