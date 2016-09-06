ba577912c39306a3eb07fc21766aabbc95d35b7b
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-06 14:20:43 +0200

    Output logs from the container during testing

--- travis/test.sh
+++ travis/test.sh
@@ -4,5 +4,6 @@ set -x
 
 DOCKER_ID=`docker ps | tail -n +2 | awk '{print $1}'`
 PASSWORD=`docker logs --tail=all $DOCKER_ID 2>&1 | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
+docker logs $DOCKER_ID
 echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
 
