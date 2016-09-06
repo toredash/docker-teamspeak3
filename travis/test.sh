fc2ad243f279a7c7de24a36891b5867bc9669e44
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-06 14:57:24 +0200

    Typo

--- travis/test.sh
+++ travis/test.sh
@@ -3,7 +3,7 @@
 set -x
 
 DOCKER_CMD="docker run --detach -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 toredash/docker-teamspeak3"
-DOCKER_ID=$DOCKER_CMD`
+DOCKER_ID=`$DOCKER_CMD`
 DOCKER_LOGS=`docker logs --tail=all $DOCKER_ID`
 PASSWORD=`echo $DOCKER_LOGS | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
 echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
