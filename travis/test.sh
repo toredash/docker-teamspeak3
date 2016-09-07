1cde55ed23ee1ad535d8c6ed34e96f8b438f5d71
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-07 12:49:43 +0200

    Optimizing test script

--- travis/test.sh
+++ travis/test.sh
@@ -2,10 +2,19 @@
 
 set -x
 
-DOCKER_CMD="docker run --detach -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 toredash/docker-teamspeak3"
-DOCKER_ID=`$DOCKER_CMD`
-sleep 15 # We need to wait for Teamspeak to start
-DOCKER_LOGS=`docker logs --tail=all $DOCKER_ID 2>&1`
-PASSWORD=`echo $DOCKER_LOGS | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
+DOCKER_ID=`docker run --detach -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 toredash/docker-teamspeak3`
+
+COUNTER=0
+while [ $COUNTER -lt 10 ]; do
+
+        DOCKER_LOGS=`docker logs --tail=all $DOCKER_ID 2>&1`
+        PASSWORD=`echo $DOCKER_LOGS | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
+        let COUNTER=$COUNTER+1
+        if [ -z "$PASSWORD" ]; then echo "No password, wait...."; else break; fi 
+        sleep 5
+done
 echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
 
+
+docker stop $DOCKER_ID
+docker rm $DOCKER_ID
