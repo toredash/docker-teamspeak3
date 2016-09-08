f855dae93b241e4177c2b1557badddeed619408a
Tore S. Lonoy <tore.lonoy@gmail.com>
2016-09-08 11:31:46 +0200

    Added a test to verify that logs are persisted between container killings

--- travis/log-testing.sh
+++ travis/log-testing.sh
@@ -2,9 +2,48 @@
 
 set -x
 
-DOCKER_ID=`docker ps --filter name=ts3_data | tail -n +2 | awk '{print $1}'`
-LOG_LINE=`docker logs $DOCKER_ID 2>/dev/null | tail -n 1`
-docker stop $DOCKER_ID
-docker start $DOCKER_ID
-LOG_LINE_DIFF=`docker logs $DOCKER_ID 2>/dev/null | tail -n 1`
+docker volume create --name logs
+DOCKER_ID=`docker run --detach -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 -v logs:/home/ts3/teamspeak3-server_linux_x86/logs toredash/docker-teamspeak3`
 
+COUNTER=0
+while [ $COUNTER -lt 10 ]; do
+
+        DOCKER_LOGS=`docker logs --tail=all $DOCKER_ID 2>&1`
+        PASSWORD=`echo $DOCKER_LOGS | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
+        let COUNTER=$COUNTER+1
+        if [ -z "$PASSWORD" ]; then echo "No password, wait...."; else break; fi 
+        sleep 5
+done
+
+LINES_BEFORE=`docker exec $DOCKER_ID ls /home/ts3/teamspeak3-server_linux_x86/logs| wc -l`
+
+
+docker stop $DOCKER_ID
+docker rm $DOCKER_ID
+
+#
+
+DOCKER_ID=`docker run --detach -p 9987:9987/udp -p 30033:30033 -p 10011:10011 -p 41144:41144 -v logs:/home/ts3/teamspeak3-server_linux_x86/logs toredash/docker-teamspeak3`
+
+COUNTER=0
+while [ $COUNTER -lt 10 ]; do
+
+        DOCKER_LOGS=`docker logs --tail=all $DOCKER_ID 2>&1`
+        PASSWORD=`echo $DOCKER_LOGS | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
+        let COUNTER=$COUNTER+1
+        if [ -z "$PASSWORD" ]; then echo "No password, wait...."; else break; fi 
+        sleep 5
+done
+
+LINES_AFTER=`docker exec $DOCKER_ID ls /home/ts3/teamspeak3-server_linux_x86/logs| wc -l`
+docker stop $DOCKER_ID
+docker rm $DOCKER_ID
+#
+
+docker volume rm logs
+
+if [[ $LINES_AFTER -gt $LINES_BEFORE ]]; then
+ exit 0
+fi
+
+exit 1
