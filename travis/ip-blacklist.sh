98c9c3bd3f60a689e70b397a39d3ea957ba5b7be
toredash <tore.lonoy@gmail.com>
2016-09-30 15:52:50 +0200

    Update ip-blacklist.sh

--- travis/ip-blacklist.sh
+++ travis/ip-blacklist.sh
@@ -17,15 +17,17 @@ while [ $COUNTER -lt 10 ]; do
         sleep 5
 done
 
-echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
+echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | tee -a /output | grep "error id=0 msg=ok"
 
 if [[ $? -ne 0 ]]; then
   echo "Unable to connect to TS3 instance..."
+  cat /output
   exit 1
 fi
 
 DOCKER_PATH=`docker volume inspect $DOCKER_VOLUME | grep Mountpoint  | cut -d"\"" -f 4`
 echo 127.0.0.1 >> $DOCKER_PATH/query_ip_blacklist.txt
+> $DOCKER_PATH/query_ip_whitelist.txt
 cat $DOCKER_PATH/query_ip_blacklist.txt
 
 docker start $DOCKER_ID
