2e80b53a092f9446608e9265f030017755dec026
toredash <tore.lonoy@gmail.com>
2016-09-30 15:55:22 +0200

    Update ip-blacklist.sh

--- travis/ip-blacklist.sh
+++ travis/ip-blacklist.sh
@@ -25,14 +25,27 @@ if [[ $? -ne 0 ]]; then
   exit 1
 fi
 
+docker stop $DOCKER_ID
+docker rm $DOCKER_ID
+
 DOCKER_PATH=`docker volume inspect $DOCKER_VOLUME | grep Mountpoint  | cut -d"\"" -f 4`
 echo 127.0.0.1 >> $DOCKER_PATH/query_ip_blacklist.txt
 > $DOCKER_PATH/query_ip_whitelist.txt
 cat $DOCKER_PATH/query_ip_blacklist.txt
 
+DOCKER_ID=`docker run --detach -p 9987:9987/udp -p 10011:10011 -v data:/ts3/data toredash/docker-teamspeak3`
 docker start $DOCKER_ID
 
-sleep 15
+COUNTER=0
+PASSWORD=""
+
+while [ $COUNTER -lt 10 ]; do
+        DOCKER_LOGS=`docker logs --tail=all $DOCKER_ID 2>&1`
+        PASSWORD=`echo $DOCKER_LOGS | egrep "password= \".*\"" -o | tr -d \" | cut -d " " -f2`
+        let COUNTER=$COUNTER+1
+        if [ -z "$PASSWORD" ]; then echo "No password, wait...."; else break; fi 
+        sleep 5
+done
 
 echo "login serveradmin $PASSWORD" | nc 127.0.0.1 10011  | grep "error id=0 msg=ok"
 
