72f68a69c6f092aef58033020345925d9b15aa2b
toredash <tore.lonoy@gmail.com>
2016-10-01 13:05:24 +0200

    Update ip-blacklist.sh

--- travis/ip-blacklist.sh
+++ travis/ip-blacklist.sh
@@ -16,6 +16,22 @@ while [ $COUNTER -lt 10 ]; do
         if [ -z "$PASSWORD" ]; then echo "No password, wait...."; else break; fi  
 done
 
+/usr/bin/expect << EOF 
+#If it all goes pear shaped the script will timeout after 5 seconds.
+set timeout 5
+set password [lindex $argv 0]
+
+#This spawns the telnet program and connects it to the variable name
+spawn telnet 127.0.0.1 10011 
+#The script expects login
+expect "TS3"
+expect "Welcome to the TeamSpeak 3 ServerQuery interface, type \"help\" for a list of commands and \"help <command>\" for information on a specific command."
+#The script sends the user variable
+send "login serveradmin $password"
+#The script expects Password
+expect "error id=0 msg=ok"
+EOF
+
 COUNTER=0
 while [ $COUNTER -lt 10 ]; do
   echo "login serveradmin $PASSWORD " | nc 127.0.0.1 10011 2>&1 | tee -a /tmp/output | grep "error id=0 msg=ok"
